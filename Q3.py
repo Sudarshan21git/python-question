990fd225e21d6fb45721d2c467fce281ed4a3f20
Sudarshan21git <sudarshansharma867@gmail.com>
2024-08-26 20:40:29 +0545

    new add

---
+++ Q3.py
@@ -0,0 +1,6 @@
+#wap to find out multiplicationof givwn number
+num=int(input("Enter the any number:"))
+
+for i in range (1,11):
+   mul=num*i
+   print(f"{num} x {i} =",mul)
---
+++ Q4.py
@@ -0,0 +1,7 @@
+# write a python program to find the sum of first natural numbers.
+n=int(input("enter the  any number:"))
+sum=0
+for i in range(n+1):
+    sum +=i
+    
+print("the sum of the first number is:",sum)
